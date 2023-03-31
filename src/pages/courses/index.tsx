aff1fffb4ec99519199abd2ba916806b4da716b5
PrashantSedhain <prashantased@gmail.com>
2023-03-30 21:03:09 -0500

    Initial shell with github workflows

---
+++ src/pages/courses/index.tsx
@@ -0,0 +1,7 @@
+export default function Courses() {
+    return (
+        <div>
+            <h3>Courses Page</h3>
+        </div>
+    )
+}
---
+++ src/pages/gallery/index.tsx
@@ -0,0 +1,7 @@
+export default function Gallery() {
+    return (
+        <div>
+            <h3>Gallery Page</h3>
+        </div>
+    )
+}
